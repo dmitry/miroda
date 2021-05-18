131126e0d32539fb4d0d8c1fd466c4eec7b8dc30
Dmitry Polushkin <dmitry.polushkin@gmail.com>
2021-05-18 22:20:24 +0300

    Cleanup app and disable not required libs

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
 require_relative "boot"
 
-require "rails/all"
+require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+# require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -16,7 +23,7 @@ module Miroda
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tallinn"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
